a4232429d9fc4636b570ca4b4a4e78de3db6f257
Shiwang9 <39117111+Shiwang9@users.noreply.github.com>
2018-06-18 00:21:09 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 
  // this will avoid mysql_connect() deprecation error.
  error_reporting( ~E_DEPRECATED & ~E_NOTICE );
- // but I strongly suggest you to use PDO or MySQLi.
+ 
  
  define('DBHOST', 'localhost');
  define('DBUSER', 'root');
